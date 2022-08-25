24ef39a867ce0b0258f15dc9812e4a6ff0490638
Dom <domwhitemedia@gmail.com>
2022-08-25 16:04:44 -0400

    building .tmx file

--- games/dw895game/CreateNewMap.java
+++ games/dw895game/CreateNewMap.java
@@ -36,7 +36,24 @@ public class CreateNewMap {
         try
         {
             FileWriter myWriter = new FileWriter("games/dw895game/map3.tmx");
-            myWriter.write("howdy");
+            myWriter.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
+            myWriter.write("\t<map version=\"1.0\" orientation=\"orthogonal\" width=\"10\" height=\"10\" tilewidth=\"64\" tileheight=\"64\">\n");
+            myWriter.write("\t\t<properties>\n");
+            myWriter.write("\t\t\t<property name=\"name\" value=\"Blackrock\"/>\n");
+            myWriter.write("\t\t</properties>\n");
+            myWriter.write("\t\t<tileset firstgid=\"1\" name=\"graphics\" tilewidth=\"64\" tileheight=\"64\">\n");
+            myWriter.write("\t\t\t<image source=\"graphics2x-basic.png\" width=\"640\" height=\"1344\"/>\n");
+            myWriter.write("\t\t</tileset>\n");
+            myWriter.write("\t\t<tileset firstgid=\"211\" name=\"walls\" tilewidth=\"64\" tileheight=\"64\">\n");
+            myWriter.write("\t\t\t<image source=\"graphics2x-walls.png\" width=\"128\" height=\"1024\"/>\n");
+            myWriter.write("\t\t</tileset>\n");
+
+
+
+
+
+
+
             myWriter.close();
             System.out.println("Successfully wrote to the file.");
         }
